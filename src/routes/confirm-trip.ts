e8ef403495e3909d024334e02a891c5b5191b143
ryandvl <contatoryandvl55@gmail.com>
2024-07-14 16:41:25 -0300

    Remove o envio de e-mail para os participantes e atualiza a WEB_BASE_URL

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -59,37 +59,37 @@ export async function confirmTrip(app: FastifyInstance) {
       const formattedStartDate = dayjs(trip.starts_at).format("LL");
       const formattedEndDate = dayjs(trip.ends_at).format("LL");
 
-      const mail = await getMailClient();
+      // const mail = await getMailClient();
 
-      await Promise.all(
-        trip.participants.map(async (participant) => {
-          const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`;
+      // await Promise.all(
+      //   trip.participants.map(async (participant) => {
+      //     const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`;
 
-          const message = await mail.sendMail({
-            from: {
-              name: "Equipe plann.er",
-              address: "oi@plann.er",
-            },
-            to: participant.email,
-            subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
-            html: `
-          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
-            <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
-            <p></p>
-            <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
-            <p></p>
-            <p>
-              <a href="${confirmationLink}">Confirmar viagem</a>
-            </p>
-            <p></p>
-            <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
-          </div>
-        `.trim(),
-          });
+      //     const message = await mail.sendMail({
+      //       from: {
+      //         name: "Equipe plann.er",
+      //         address: "oi@plann.er",
+      //       },
+      //       to: participant.email,
+      //       subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+      //       html: `
+      //     <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+      //       <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+      //       <p></p>
+      //       <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+      //       <p></p>
+      //       <p>
+      //         <a href="${confirmationLink}">Confirmar viagem</a>
+      //       </p>
+      //       <p></p>
+      //       <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+      //     </div>
+      //   `.trim(),
+      //     });
 
-          console.log(nodemailer.getTestMessageUrl(message));
-        })
-      );
+      //     console.log(nodemailer.getTestMessageUrl(message));
+      //   })
+      // );
 
       return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`);
     }
--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -71,36 +71,36 @@ export async function createTrip(app: FastifyInstance) {
       const formattedStartDate = dayjs(starts_at).format("LL");
       const formattedEndDate = dayjs(ends_at).format("LL");
 
-      const confirmationLink = `${env.API_BASE_URL}/trips/${trip.id}/confirm`;
+      // const confirmationLink = `${env.API_BASE_URL}/trips/${trip.id}/confirm`;
 
-      const mail = await getMailClient();
+      // const mail = await getMailClient();
 
-      const message = await mail.sendMail({
-        from: {
-          name: "Equipe plann.er",
-          address: "oi@plann.er",
-        },
-        to: {
-          name: owner_name,
-          address: owner_email,
-        },
-        subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
-        html: `
-          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
-            <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
-            <p></p>
-            <p>Para confirmar sua viagem, clique no link abaixo:</p>
-            <p></p>
-            <p>
-              <a href="${confirmationLink}">Confirmar viagem</a>
-            </p>
-            <p></p>
-            <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
-          </div>
-        `.trim(),
-      });
+      // const message = await mail.sendMail({
+      //   from: {
+      //     name: "Equipe plann.er",
+      //     address: "oi@plann.er",
+      //   },
+      //   to: {
+      //     name: owner_name,
+      //     address: owner_email,
+      //   },
+      //   subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
+      //   html: `
+      //     <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+      //       <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+      //       <p></p>
+      //       <p>Para confirmar sua viagem, clique no link abaixo:</p>
+      //       <p></p>
+      //       <p>
+      //         <a href="${confirmationLink}">Confirmar viagem</a>
+      //       </p>
+      //       <p></p>
+      //       <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+      //     </div>
+      //   `.trim(),
+      // });
 
-      console.log(nodemailer.getTestMessageUrl(message));
+      // console.log(nodemailer.getTestMessageUrl(message));
 
       return { tripId: trip.id };
     }
